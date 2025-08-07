1581d63d4fb0d003ebd8e5ad92d13407f7496b00
BEATRIZ JULIÃO BOLETINI DA SILVA <24122273@dsn127m1247444.cfp127aluno.local>
2025-08-07 12:32:07 -0300

    ok

--- modulos/api.js
+++ modulos/api.js
@@ -1,10 +1,8 @@
 'use strict'
 
-export const getProjetoFotos = async () => {
-    const url = 'http://localhost:3000/fotos'
-    const response = await fetch(url)
-    const data = await response.json()
-
-    console.log (data)
+export const getPhotos = async () => {
+  const url = 'http://localhost:3000/fotos'
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
 }
-
